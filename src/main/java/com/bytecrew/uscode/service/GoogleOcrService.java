a33171e45031ecf0b043bd9589b86ff9a469583b
2heunxun <seapeon@naver.com>
2025-06-28 03:31:57 +0900

    Feat : Ocr Dev

--- src/main/java/com/bytecrew/uscode/service/GoogleOcrService.java
+++ src/main/java/com/bytecrew/uscode/service/GoogleOcrService.java
@@ -5,12 +5,16 @@ import com.bytecrew.uscode.dto.OcrExtractResponseDto;
 import com.bytecrew.uscode.dto.UserSaveRequestDto;
 import com.bytecrew.uscode.repository.UserRepository;
 import com.bytecrew.uscode.util.AES256Util;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.vision.v1.*;
 import com.google.protobuf.ByteString;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
 import java.util.List;
 
 @Service
@@ -19,16 +23,24 @@ public class GoogleOcrService {
 
     private final UserRepository userRepository;
 
+    @Value("${google.credentials.path}")
+    private String credentialsPath;
+
     public OcrExtractResponseDto extractOnly(MultipartFile file) {
-        try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+        try (ImageAnnotatorClient vision = createVisionClient()) {
+            System.out.println(vision);
             ByteString imgBytes = ByteString.copyFrom(file.getBytes());
+            System.out.println(imgBytes);
 
             Image img = Image.newBuilder().setContent(imgBytes).build();
+            System.out.println(img);
             Feature feat = Feature.newBuilder().setType(Feature.Type.TEXT_DETECTION).build();
+            System.out.println(feat);
             AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
                     .addFeatures(feat)
                     .setImage(img)
                     .build();
+            System.out.println(request);
 
             AnnotateImageResponse response = vision.batchAnnotateImages(List.of(request))
                     .getResponsesList().get(0);
@@ -65,7 +77,13 @@ public class GoogleOcrService {
         }
         return "이름없음";
     }
-
+    private ImageAnnotatorClient createVisionClient() throws Exception {
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
+        ImageAnnotatorSettings settings = ImageAnnotatorSettings.newBuilder()
+                .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
+                .build();
+        return ImageAnnotatorClient.create(settings);
+    }
     private String extractJumin(String text) {
         return text.replaceAll("[^0-9-]", "")
                 .replaceAll(".*?(\\d{6}-\\d{7}).*", "$1");
