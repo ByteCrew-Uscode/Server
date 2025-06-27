685f51c7ef4376cf35f29ffbf889a477fc62ab3f
L33gn21 <l33gn21@gmail.com>
2025-06-27 15:23:37 +0900

    init

---
+++ src/main/java/com/bytecrew/uscode/UscodeApplication.java
@@ -0,0 +1,13 @@
+package com.bytecrew.uscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UscodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UscodeApplication.class, args);
+    }
+
+}
